32b6fbb2bcd959c574404f0ac797ca666578a272
Roni Choudhury <roni.choudhury@kitware.com>
2014-10-14 17:39:39 -0400

    Adding qunit testing with Tangelo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,20 @@
+/*jshint node:true*/
 /*global module:false*/
 module.exports = function(grunt) {
+  "use strict";
 
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    pkg: grunt.file.readJSON("package.json"),
+    banner: "/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - " +
+      "<%= grunt.template.today(\"yyyy-mm-dd\") %>\n" +
+      "<%= pkg.homepage ? \"* \" + pkg.homepage + \"\\n\" : \"\" %>" +
+      "* Copyright (c) <%= grunt.template.today(\"yyyy\") %> <%= pkg.author.name %>;" +
+      " Licensed <%= _.pluck(pkg.licenses, \"type\").join(\", \") %> */\n",
     // Task configuration.
     version: {
-        src: ['lib/core/core.js']
+        src: ["lib/core/core.js"]
     },
     cleanup: {
         dist: ["dist"],
@@ -20,22 +22,21 @@ module.exports = function(grunt) {
     },
     concat: {
       options: {
-        banner: '<%= banner %>',
+        banner: "<%= banner %>",
         stripBanners: true
       },
       dist: {
-        //src: ['lib/<%= pkg.name %>.js'],
-        src: ['lib/**/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
+        src: ["lib/**/*.js"],
+        dest: "dist/<%= pkg.name %>.js"
       }
     },
     uglify: {
       options: {
-        banner: '<%= banner %>'
+        banner: "<%= banner %>"
       },
       dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
+        src: "<%= concat.dist.dest %>",
+        dest: "dist/<%= pkg.name %>.min.js"
       }
     },
     jshint: {
@@ -75,45 +76,145 @@ module.exports = function(grunt) {
         globals: {}
       },
       gruntfile: {
-        src: 'Gruntfile.js'
+        src: "Gruntfile.js"
       },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
+      libTest: {
+        src: ["lib/**/*.js", "test/**/*.js"]
       }
     },
-    qunit: {
-      files: ['test/**/*.html']
+    qunitTests: {
+      options: {
+        httpBase: "http://localhost:8080"
+      },
+      files: ["test/**/*.html"]
     },
     watch: {
       gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
+        files: "<%= jshint.gruntfile.src %>",
+        tasks: ["jshint:gruntfile"]
       },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
+      libTest: {
+        files: "<%= jshint.lib_test.src %>",
+        tasks: ["jshint:libTest", "qunit"]
       }
     }
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-version');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks("grunt-continue");
+  grunt.loadNpmTasks("grunt-version");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-qunit");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-clean");
 
   // Default task.
-  grunt.registerTask('default', ['version', 'jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask("default", ["version", "jshint", "qunit", "concat", "uglify"]);
+
+  // Tangelo launch/kill task.
+  (function () {
+    var tangelo = null,
+        stopping,
+        output = [];
+
+    grunt.registerTask("tangelo", "Starts/stops a Tangelo server instance.", function (op) {
+      var done,
+          fragment = null;
+
+      if (op === "start") {
+        if (tangelo) {
+          grunt.fail.warn("Tangelo is running already.");
+        }
+
+        stopping = false;
+
+        done = this.async();
+
+        tangelo = grunt.util.spawn({
+          cmd: "tangelo",
+          args: [
+            "--port", "8080",
+            "--root", "."
+          ]
+        }, function () {});
+
+        tangelo.stderr.setEncoding("utf8");
+
+        tangelo.stderr.on("data", function (chunk) {
+          var complete = chunk.slice(-1) === "\n",
+              lines,
+              i,
+              n;
+
+          if (fragment) {
+            chunk = fragment + chunk;
+            fragment = null;
+          }
+
+          lines = chunk.split("\n");
+          n = complete ? lines.length : lines.length - 1;
+          for (i = 0; i < n; i++) {
+            if (!stopping && lines[i].indexOf("ENGINE Bus STARTED") !== -1) {
+              console.log("Tangelo started with PID " + tangelo.pid);
+              done();
+            }
+
+            output.push(lines[i]);
+          }
+
+          if (!complete) {
+            fragment = lines[lines.length - 1];
+          }
+        });
+
+        tangelo.stderr.on("end", function () {
+          if (stopping) {
+              return;
+          }
+          grunt.fail.fatal("Tangelo could not be started\n" + output.join("\n"));
+        });
+      } else if (op === "stop") {
+        if (!tangelo) {
+          grunt.fail.warn("Tangelo is not running");
+        }
+
+        stopping = true;
+        done = this.async();
+
+        tangelo.kill();
+
+        tangelo.stderr.on("end", function () {
+          done();
+        });
+
+        setTimeout(function () {
+          grunt.fail.warn("Could not kill Tangelo\n" + output.join("\n"));
+        }, 10000);
+      } else {
+        grunt.fail.warn("Unknown argument: '" + op + "'");
+      }
+    });
+  }());
+
+  // Test task.  Start the Tangelo server, run the tests, then shut the server
+  // down.
+  grunt.renameTask("qunit", "qunitTests");
+  grunt.registerTask("qunit", [
+    "tangelo:start",
+    "continueOn",
+    "qunitTests",
+    "continueOff",
+    "tangelo:stop"
+  ]);
 
   // Clean task.  To prevent "grunt clean" from deleting the node_modules
   // directory (as it would by default), we're renaming the task to "cleanup",
   // then installing a better behaved default "clean" task.
   grunt.renameTask("clean", "cleanup");
-  grunt.registerTask('clean', ['clean:dist']);
-  grunt.registerTask('clean:dist', ['cleanup:dist']);
-  grunt.registerTask('clean:dev', ['cleanup:dev']);
-  grunt.registerTask('clean:all', ['clean:dist', 'clean:dev']);
+  grunt.registerTask("clean", ["clean:dist"]);
+  grunt.registerTask("clean:dist", ["cleanup:dist"]);
+  grunt.registerTask("clean:dev", ["cleanup:dev"]);
+  grunt.registerTask("clean:all", ["clean:dist", "clean:dev"]);
 };
